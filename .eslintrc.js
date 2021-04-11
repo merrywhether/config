7c404d4146ef22c339849e854df0a14095c9c529
merrywhether <risto.k@mac.com>
2021-04-11 12:42:44 -0700

    Bump deps and add new available rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -53,6 +53,7 @@ module.exports = {
     '@typescript-eslint/no-confusing-void-expression': 'error',
     '@typescript-eslint/no-unnecessary-boolean-literal-compare': 'error',
     '@typescript-eslint/no-unnecessary-condition': 'error',
+    '@typescript-eslint/no-unsafe-argument': 'error',
     '@typescript-eslint/prefer-includes': 'error',
     '@typescript-eslint/prefer-nullish-coalescing': 'error',
     '@typescript-eslint/prefer-reduce-type-parameter': 'error',
@@ -90,6 +91,10 @@ module.exports = {
       rules: {
         'import/no-default-export': 'off',
       },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {},
     },
   ],
 };
