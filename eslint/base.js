0458b3989383afddde363087709c0a137827721b
Risto Keravuori <risto.k@mac.com>
2021-10-23 15:27:42 -0700

    Add sourceType to base ESLint config (#22)

--- eslint/base.js
+++ eslint/base.js
@@ -4,6 +4,9 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  parserOptions: {
+    sourceType: 'module',
+  },
   reportUnusedDisableDirectives: true,
   plugins: ['import', 'sort-destructure-keys', 'sort-keys-fix'],
   extends: ['eslint:recommended'],
