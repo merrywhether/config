ebfc980b32022a1c4f8ef8d3bcc1a5198baeff75
merrywhether <r@risto.io>
2024-04-16 19:13:43 -0700

    Switch all sorting to perfectionist

--- src/eslint/rules.js
+++ src/eslint/rules.js
@@ -1,40 +1,30 @@
+// https://eslint.org/docs/latest/rules/
 const eslintRules = {
   eqeqeq: ['error', 'always', { null: 'ignore' }],
-  'no-unused-expressions': ['error', { enforceForJSX: true }],
+  // https://eslint.org/docs/latest/rules/no-unused-expressions
+  'no-unused-expressions': [
+    'error',
+    { allowShortCircuit: true, enforceForJSX: true },
+  ],
+  // https://eslint.org/docs/latest/rules/no-useless-computed-key
   'no-useless-computed-key': 'error',
   'prefer-const': 'error',
+  // https://eslint.org/docs/latest/rules/prefer-template
   'prefer-template': 'error',
-  'sort-imports': ['error', { ignoreDeclarationSort: true }],
+  // https://eslint.org/docs/latest/rules/symbol-description
   'symbol-description': 'error',
   'valid-typeof': ['error', { requireStringLiterals: true }],
 };
 
+// https://github.com/import-js/eslint-plugin-import
 const importRules = {
   'import/first': 'error',
   'import/newline-after-import': 'error',
-  'import/no-cycle': 'error',
   'import/no-duplicates': ['error', { 'prefer-inline': true }],
-  'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
-  'import/order': [
-    'error',
-    {
-      alphabetize: {
-        order: 'asc',
-        orderImportKind: 'asc',
-      },
-      'newlines-between': 'never',
-    },
-  ],
+  'import/no-useless-path-segments': ['error'],
 };
 
-const sortKeysRules = {
-  'sort-destructure-keys/sort-destructure-keys': [
-    'error',
-    { caseSensitive: true },
-  ],
-  'sort-keys-fix/sort-keys-fix': ['error', 'asc', { natural: true }],
-};
-
+// https://typescript-eslint.io/rules/
 const tsBaseRules = {
   '@typescript-eslint/consistent-type-imports': [
     'error',
@@ -43,27 +33,13 @@ const tsBaseRules = {
   '@typescript-eslint/default-param-last': 'error',
   '@typescript-eslint/explicit-function-return-type': 'error',
   '@typescript-eslint/explicit-module-boundary-types': 'error',
-  // pending https://github.com/typescript-eslint/typescript-eslint/issues/2296
-  // '@typescript-eslint/member-ordering': [
-  //   'error',
-  //   {
-  //     default: { optionalityOrder: 'required-first', order: 'alphabetically' },
-  //   },
-  // ],
   '@typescript-eslint/no-import-type-side-effects': 'error',
   '@typescript-eslint/no-shadow': 'error',
   '@typescript-eslint/no-unused-expressions': [
     'error',
-    { enforceForJSX: true },
+    { allowShortCircuit: true, enforceForJSX: true },
   ],
-  '@typescript-eslint/sort-type-constituents': 'error',
   'no-unused-expressions': 'off',
-  'typescript-sort-keys/interface': [
-    'error',
-    // `requiredFirst: true` is only non-default value
-    'asc',
-    { caseSensitive: true, natural: false, requiredFirst: true },
-  ],
 };
 
 const tsTypeRules = {
@@ -88,7 +64,6 @@ const reactRules = {
   'react/jsx-no-constructed-context-values': 'error',
   'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
   'react/jsx-pascal-case': 'error',
-  'react/jsx-sort-props': 'error',
   'react/no-children-prop': 'off',
   'react/no-invalid-html-attribute': 'error',
   'react/prop-types': 'off',
@@ -99,7 +74,6 @@ const solidReactRules = {
   'react/jsx-boolean-value': 'error',
   'react/jsx-no-target-blank': 'error',
   'react/jsx-pascal-case': 'error',
-  'react/jsx-sort-props': 'error',
   'react/no-unescaped-entities': 'error',
 };
 
@@ -108,7 +82,6 @@ export const rules = {
   import: importRules,
   react: reactRules,
   solidReact: solidReactRules,
-  sortKeys: sortKeysRules,
   tsBase: tsBaseRules,
   tsType: tsTypeRules,
 };
